35eb3a51c3566616c14b63d0a50f668b53eccd82
Cheng Vue <cvu3@ucsc.edu>
2019-01-23 00:56:54 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,6 @@
 class PostsController < ApplicationController
 	def index
 		@posts = Post.all
-		<% @posts.each do |post| %>
-	  	<div><a href='<%= "/posts/#{post.id}" %>'><%= post.title %></a></div>
-		<% end %>
 	end
 
 	def show
